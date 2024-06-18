0581f2e684b3462f02bfc997a606da300e990b51
Maksym Operchuk <operlast529@gmail.com>
2024-06-18 12:40:16 +0300

    Update main.py: add comments where needed.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from services import exception_handler
 
 app = FastAPI()
 
+# Initialize the summarization pipeline.
 summarization_pipeline = pipeline(
     "summarization", model="sshleifer/distilbart-cnn-12-6"
 )
@@ -13,7 +14,11 @@ summarization_pipeline = pipeline(
 
 @app.post("/summarize")
 @exception_handler
-async def summarize_text(text_input: TextInput):
+async def summarize_text(text_input: TextInput) -> dict:
+    """
+    Endpoint to summarize the input text using a pre-trained summarization model.
+    """
+
     summary = summarization_pipeline(
         text_input.text, max_length=150, min_length=50, do_sample=False
     )
