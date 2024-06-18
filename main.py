78aeb0b36cf6f616d7b7911f81f896c794f37f8b
Maksym Operchuk <operlast529@gmail.com>
2024-06-18 11:51:38 +0300

    Update main.py: implement summarize_text view.

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from schemas import TextInput
 from transformers import pipeline
 
 
 app = FastAPI()
 
 summarization_pipeline = pipeline("summarization")
+
+
+@app.post("/summarize")
+async def summarize_text(text_input: TextInput):
+    summary = summarization_pipeline(
+        text_input.text, max_length=150, min_length=50, do_sample=False
+    )
+
+    return {"summary": summary[0]["summary_text"]}
