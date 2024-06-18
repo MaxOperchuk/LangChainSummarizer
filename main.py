bacb7e7ca8f04bafeeee57aa14aad06e19611932
Maksym Operchuk <operlast529@gmail.com>
2024-06-18 13:48:51 +0300

    Update main.py: add using of HuggingFacePipeline model.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from schemas import TextInput
 from transformers import pipeline
 from services import exception_handler
+from langchain_huggingface import HuggingFacePipeline
 
 
 app = FastAPI()
@@ -11,6 +12,9 @@ summarization_pipeline = pipeline(
     "summarization", model="sshleifer/distilbart-cnn-12-6"
 )
 
+# Wrap the pipeline in a LangChain model
+summarizer = HuggingFacePipeline(pipeline=summarization_pipeline)
+
 
 @app.post("/summarize")
 @exception_handler
@@ -19,7 +23,5 @@ async def summarize_text(text_input: TextInput) -> dict:
     Endpoint to summarize the input text using a pre-trained summarization model.
     """
 
-    summary = summarization_pipeline(
-        text_input.text, max_length=150, min_length=50, do_sample=False
-    )
-    return {"summary": summary[0]["summary_text"]}
+    summary = summarizer(text_input.text)
+    return {"summary": summary}
